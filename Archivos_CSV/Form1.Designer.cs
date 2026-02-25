89404765c22f2cb9f96cca78ddea491226083554
CaroS <CaroS@MSICAROLINASUST>
2026-02-24 21:29:49 -0600

    Agregar archivos de proyecto.

---
+++ Archivos_CSV/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Archivos_CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(47, 125);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.RowHeadersWidth = 51;
+            this.dgvDatos.RowTemplate.Height = 24;
+            this.dgvDatos.Size = new System.Drawing.Size(853, 479);
+            this.dgvDatos.TabIndex = 0;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(239)))), ((int)(((byte)(189)))));
+            this.btnAbrir.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrir.Location = new System.Drawing.Point(50, 66);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(138, 38);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(211)))), ((int)(((byte)(133)))));
+            this.btnCrear.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrear.Location = new System.Drawing.Point(194, 66);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(138, 38);
+            this.btnCrear.TabIndex = 2;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = false;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(209)))), ((int)(((byte)(240)))));
+            this.btnModificar.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(338, 66);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(138, 38);
+            this.btnModificar.TabIndex = 3;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(161)))), ((int)(((byte)(247)))));
+            this.btnEliminar.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(482, 66);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(138, 38);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(130)))), ((int)(((byte)(153)))));
+            this.btnGuardar.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(779, 66);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(138, 38);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(320, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 34);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Archivos CSV";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(204)))), ((int)(((byte)(234)))));
+            this.ClientSize = new System.Drawing.Size(957, 631);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.dgvDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Archivos_CSV/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Archivos_CSV
+{
+    public partial class Form1 : Form
+    {
+        DataTable tablaCSV = new DataTable();
+        string rutaArchivoActual = "";
+        private object saveFileDialog;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos CSV (*.csv)|*.csv|Todos los archivos (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                rutaArchivoActual = openFileDialog.FileName; // <-- AGREGA ESTA LÍNEA
+            {
+                tablaCSV.Clear();
+                tablaCSV.Columns.Clear();
+                string[] lineas = System.IO.File.ReadAllLines(openFileDialog.FileName);
+
+                if (lineas.Length > 0)
+                {
+                    string[] encabezados = lineas[0].Split(',');
+                    foreach (string encabezado in encabezados)
+                    {
+                        tablaCSV.Columns.Add(encabezado);
+                    }
+                    for (int i = 1; i < lineas.Length; i++)
+                    {
+                        string[] datos = lineas[i].Split(',');
+                        tablaCSV.Rows.Add(datos);
+                    }
+                    dgvDatos.DataSource = tablaCSV;
+                    // --- NUEVO CÓDIGO: Bloquear todas las celdas cargadas ---
+                    foreach (DataGridViewRow fila in dgvDatos.Rows)
+                    {
+                        foreach (DataGridViewCell celda in fila.Cells)
+                        {
+                            celda.ReadOnly = true;
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Archivos CSV (*.csv)|*.csv";
+            saveFileDialog.Title = "Crear nuevo archivo CSV";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Guardamos la ruta del nuevo archivo
+                rutaArchivoActual = saveFileDialog.FileName;
+
+                // Limpiamos la tabla en pantalla
+                tablaCSV.Clear();
+                tablaCSV.Columns.Clear();
+
+                // Creamos unas columnas por defecto para poder empezar a escribir
+                // Puedes cambiar estos nombres por los que necesites
+                tablaCSV.Columns.Add("Columna1");
+                tablaCSV.Columns.Add("Columna2");
+                tablaCSV.Columns.Add("Columna3");
+
+                // Creamos el archivo físico vacío (solo con los encabezados)
+                using (StreamWriter sw = new StreamWriter(rutaArchivoActual))
+                {
+                    sw.WriteLine("Columna1,Columna2,Columna3");
+                }
+                // 5. ¡EL TRUCO AQUÍ! Refrescamos el DataGridView
+                dgvDatos.DataSource = null; // Quitamos lo que había
+                dgvDatos.DataSource = tablaCSV; // Ponemos la nueva tabla limpia
+
+                MessageBox.Show("Archivo nuevo creado. Ya puedes empezar a agregar datos en la tabla.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            // Verifica que haya una fila seleccionada y que no sea la fila vacía de nuevo ingreso
+            if (dgvDatos.CurrentCell != null && !dgvDatos.Rows[dgvDatos.CurrentRow.Index].IsNewRow)
+            {
+                dgvDatos.Rows.RemoveAt(dgvDatos.CurrentRow.Index);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona una fila válida para eliminar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void dgvDatos_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            // Al momento de que el usuario termina de escribir, bloqueamos esa celda específica
+            dgvDatos.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly = true;
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (dgvDatos.CurrentCell != null && !dgvDatos.Rows[dgvDatos.CurrentRow.Index].IsNewRow)
+            {
+                dgvDatos.CurrentCell.ReadOnly = false;
+                dgvDatos.BeginEdit(true);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona una fila válida para modificar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+            // 1. Aseguramos que cualquier edición en la celda se confirme
+            dgvDatos.EndEdit();
+            tablaCSV.AcceptChanges();
+
+            // 2. Verificamos si realmente hay un archivo abierto
+            if (string.IsNullOrEmpty(rutaArchivoActual))
+            {
+                MessageBox.Show("No hay ningún archivo abierto para guardar. Primero abre un archivo CSV.",
+                                "Aviso",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                return; // Salimos del método para no causar un error
+            }
+
+            try
+            {
+                // 3. Sobrescribimos el archivo original directamente
+                using (StreamWriter sw = new StreamWriter(rutaArchivoActual))
+                {
+                    // Escribir los encabezados
+                    for (int i = 0; i < tablaCSV.Columns.Count; i++)
+                    {
+                        sw.Write(tablaCSV.Columns[i].ColumnName);
+                        if (i < tablaCSV.Columns.Count - 1)
+                        {
+                            sw.Write(",");
+                        }
+                    }
+                    sw.WriteLine();
+
+                    // Escribir las filas
+                    foreach (DataRow fila in tablaCSV.Rows)
+                    {
+                        for (int i = 0; i < tablaCSV.Columns.Count; i++)
+                        {
+                            sw.Write(fila[i].ToString());
+                            if (i < tablaCSV.Columns.Count - 1)
+                            {
+                                sw.Write(",");
+                            }
+                        }
+                        sw.WriteLine();
+                    }
+                }
+
+                MessageBox.Show("Los cambios se han guardado en el archivo original.",
+                                "Guardado Rápido Exitoso",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al intentar guardar el archivo:\n\n" + ex.Message,
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+ 
